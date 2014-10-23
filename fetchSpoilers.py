ad89748540ed786e5bfaf84ff615a8e9958b4bf8
Allen Sarkisyan <programminglinguist@gmail.com>
2014-10-23 02:43:20 -0700

    Fixed duplicate problem, made a new problem
    Not really. The script sometimes stops with an error about utf-8
    character insertion into the database. Weird, I tried to fix it but it
    still sucks. No matter, lots of good data is received.

--- fetchSpoilers.py
+++ fetchSpoilers.py
@@ -1,7 +1,6 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import sqlite3
-import re
 
 conn = sqlite3.connect('moviespoilerbot.db')
 c = conn.cursor()
@@ -11,6 +10,8 @@ spoilerSite = 'http://www.spollywood.com/'
 def weirdTextFix(str):
 	str = str.replace("&amp;","&")#replace stuff with ampersand sign
 	str = str.replace("&#39;","''")#replace stuff with 2 single quotes (necessary for sql insertion)
+	str = str.replace("(","")
+	str = str.replace(")","")
 	return str
 
 def getSite():
@@ -26,16 +27,32 @@ def parsePage(html_page):
 	spoiler = spoiler[spoiler.index('>')+1:spoiler.index('</')]
 	title = weirdTextFix(title)
 	spoiler = weirdTextFix(spoiler)
-	print title
-	print spoiler
+	return (title,spoiler)
 	
-	if(len(spoiler)<=140):#to be tweetable
-		tablename = "table_"+title[0]
-		c.execute("create table if not exists "+tablename+"(ID INTEGER PRIMARY KEY, TITLE text, SPOILER text)")
-		c.execute("insert into "+tablename+" (TITLE,SPOILER) values (?,?)",[title,spoiler])
-		conn.commit()
+def insertToDB(title,spoiler):
+	valid_utf8 = True #because mysql only takes utf-8 and too lazy to change that, but still runs into problem sometimes? what the hell...
+	try:
+		title.decode('utf-8')
+		spoiler.decode('utf-8')
+	except UnicodeDecodeError:
+		valid_utf8 = False
+	
+	if valid_utf8:
+		if(len(spoiler)<=140):#to be tweetable
+			tablename = "table_"+title[0]
+			c.execute("create table if not exists "+tablename+"(ID INTEGER PRIMARY KEY, TITLE text, SPOILER text)")
+			conn.commit()
+			c.execute("select 1 from "+tablename+" where TITLE=:title",{"title":title})
+			conn.commit()
+			if c.fetchone() is None:
+					c.execute("insert into "+tablename+" (TITLE,SPOILER) values (:title,:spoiler)",{"title":title,"spoiler":spoiler})
+					print title
+					print spoiler
+					conn.commit()
+	
 
-for i in range(5):
-	parsePage(getSite())
+for i in range(300):
+	t,s = parsePage(getSite())
+	insertToDB(t,s)
 
 conn.close()
