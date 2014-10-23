7774e881ea39ec9b9ffa5e1ad106d8771730ffb2
Allen Sarkisyan <programminglinguist@gmail.com>
2014-10-23 01:31:38 -0700

    fixed weird text, limited character count to 140
    seems to work fine, all that is left is checking for unique entries

--- fetchSpoilers.py
+++ fetchSpoilers.py
@@ -1,11 +1,17 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import sqlite3
+import re
 
 conn = sqlite3.connect('moviespoilerbot.db')
 c = conn.cursor()
 
 spoilerSite = 'http://www.spollywood.com/'
+
+def weirdTextFix(str):
+	str = str.replace("&amp;","&")#replace stuff with ampersand sign
+	str = str.replace("&#39;","''")#replace stuff with 2 single quotes (necessary for sql insertion)
+	return str
 
 def getSite():
 	return urllib2.urlopen(spoilerSite).read()
@@ -18,14 +24,18 @@ def parsePage(html_page):
 	spoiler = soup.findAll(attrs={'class':'spoiler-text'})
 	spoiler = str(spoiler)
 	spoiler = spoiler[spoiler.index('>')+1:spoiler.index('</')]
+	title = weirdTextFix(title)
+	spoiler = weirdTextFix(spoiler)
+	print title
+	print spoiler
 	
-	if(len(spoiler)<=140):
+	if(len(spoiler)<=140):#to be tweetable
 		tablename = "table_"+title[0]
 		c.execute("create table if not exists "+tablename+"(ID INTEGER PRIMARY KEY, TITLE text, SPOILER text)")
 		c.execute("insert into "+tablename+" (TITLE,SPOILER) values (?,?)",[title,spoiler])
 		conn.commit()
 
-for i in range(50):
+for i in range(5):
 	parsePage(getSite())
 
 conn.close()
