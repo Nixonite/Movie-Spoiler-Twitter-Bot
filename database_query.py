e8f1ccb406c5946385a61ae32bd0be9bca8e8aef
Allen Sarkisyan <programminglinguist@gmail.com>
2014-11-04 20:18:37 -0800

    Stemming, dynamic reggae generation
    I think it works, but still need to test it on server but server
    doesn’t have nltk yet so one more commit after this.

--- database_query.py
+++ database_query.py
@@ -1,4 +1,6 @@
-movies =['The Terminator', 'The Lion King', 'Princess Mononoke', 'Twilight', 'Frozen', 'Dracula', 'Guardians of the Galaxy']
+import nltk
+import pymongo
+import re
 
 
 def makeRegex(movie):
@@ -9,29 +11,35 @@ def makeRegex(movie):
 		else:
 			regex+= "["+i.lower() + i.upper() + "]"
 	return regex
-	
-	
-movieRegex = []
-for i in movies:
-	movieRegex.append(makeRegex(i))
-
-import nltk
-stemmer = nltk.WordNetLemmatizer()
-tweet = "basldfjasdf oij sdfoij asdf oi asdfoj asf"
-lemmas = []
-tweetWords = tweet.split()
-for w in tweetWords:
-	lemmas.append(stemmer.lemmatize(w))
-
-for i in lemmas:
-	if i == "see" or i == "watch" or i == "catch":
-		return "Yay"
-		break;
-
-'''import pymongo
 
 conn = pymongo.MongoClient()
 
 db = conn.twitter
 
-[ tweet.find_one({'text':{'$regex':movieRegex[i]}})['text'] for i in moviesRegex]'''
+twitterDB = db.lines
+
+movies =["Twilight", "Frozen", "Dracula", "Guardians of the Galaxy"]
+movieRegex = []
+for i in movies:
+	movieRegex.append(makeRegex(i))
+
+
+for i in moviesRegex:
+	PossibleTweetList = list(twitterDB.find({"text":{"$regex":i}})["text"].limit(20))
+	for tweet in PossibleTweetList:
+		stemmer = nltk.WordNetLemmatizer()
+		lemmas = []
+		tweetWords = tweet.split()
+		for w in tweetWords:
+			lemmas.append(stemmer.lemmatize(w))
+
+		for i in lemmas:
+			if i == "see" or i == "watch" or i == "catch":
+				print "Yay",lemmas[lemmas.index(i)]
+				break;
+
+
+
+
+
+
