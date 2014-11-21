ba9e6bd533bbb068c54f31eae599130d00273014
Allen Sarkisyan <programminglinguist@gmail.com>
2014-11-20 23:16:58 -0800

    checks against duplicates successfully

--- database_query.py
+++ database_query.py
@@ -32,6 +32,10 @@ def sqlClose(SQLCONN):
 	print "Closing SQL..."
 	SQLCONN.close()
 
+def noDuplicatesSetup(sqlc):
+	sqlc.execute("create table if not exists Replied_Tweets (ID INTEGER PRIMARY KEY, tweetID VARCHAR(255))")
+	sqlc.commit()
+
 
 ###############################################################
 
@@ -75,39 +79,66 @@ def rememberTheMovie(text,tupleMR):
 ###############################################################
 
 
-def regexFilter(tweet):#takes out false positive tweets, also assumes tweet is lowercase
+def regexFilter(tweet):#trivial time
 	text = tweet['text'].lower()
 	if re.search("watched|saw|again|seen|watches|was|great|after|seat|back\s*from\s*seeing|from\s*watching|while\s*watching|fantastic|amazing|cool|favorite|good|went|were|second\s*time|produced|funny|sad|recommend",text) is None:
 	#the above regex excludes stuff like fantastic/great/amazing etc. because they often come in the form of a review of the film i.e. already watched it.
 		if re.search("watching|gonna\s*see|gonna\s*watch|wanna\s*see|want\s*to\s*see|going\s*to\s*see|seeing|going\s*to\s*watch|wanna\s*watch|wanna\s*go\s*see|want\s*to\s*go\s*see|I\s*need\s*to\s*see|I\s*have\s*to\s*watch|want\s*to\s*watch|seeing",text):
 			return True
 	return False
+	
+
+###############################################################
+
 
 def spoil(tweet,title,sqlc):#the act of evil
 	tablename = "table_"+title[0]
 	sqlc.execute("select spoiler from "+tablename+" where TITLE=:title",{"title":title})
 	spoiler = sqlc.fetchone()
 	print tweet['id_str'],tweet['text'],"\n","Movie:",title,"\t Spoiler:",spoiler,"\n"
+	
 
-def query(bigMovieRegex,twitterDB,sqlc):#maybe needs a better name since it both queries the mongodb and spoils
+###############################################################
+
+
+def noDuplicate(id_str,sqlCURSOR):
+	sqlCURSOR.execute("select tweetID from Replied_Tweets where tweetID=:tID",{"tID":id_str})
+	result = sqlCURSOR.fetchone()
+	if result is None:
+		return True
+	else: return False
+
+def insertDuplicatesTable(id_str,sqlCURSOR,sqlCONN):
+	sqlCURSOR.execute("insert into Replied_Tweets (tweetID) values (:tID)",{"tID":id_str})
+	sqlCONN.commit()
+
+###############################################################
+
+
+def query(bigMovieRegex,twitterDB,sqlCURSOR,sqlCONN):#maybe needs a better name since it both queries the mongodb and spoils
 
 	global howMuchToSearch
+	global matchedCounter
 	
 	startT = time.time()
 	print "\n\n\nStarting a search in the database for ",howMuchToSearch," possible tweets..."
 	PossibleTweetList = list(twitterDB.find({"text":{"$regex":bigMovieRegex}}).sort([['id_str', 1]]).limit(howMuchToSearch))# sorting by str_id is faster than _id
 
 	print "Time it took: ",time.time()-startT
-	print "Done. Now seeing which match..."
+	print "Done. Now seeing if they match..."
 	
+	movieRegTupe = tupleMoviesRegex(movies,moviesToRegexList(movies))
 	for tweet in PossibleTweetList:
-		
-		if regexFilter(tweet):
-			title = rememberTheMovie(tweet['text'],tupleMoviesRegex(movies,moviesToRegexList(movies)))
-			spoil(tweet,title,sqlc)
-			#print tweet['id_str'],tweet['text'],'\n','Movie: no idea \t Spoiler: no clue\n'
-			global matchedCounter
-			matchedCounter+=1
+		if noDuplicate(tweet['id_str'],sqlCURSOR):
+			if regexFilter(tweet):
+				title = rememberTheMovie(tweet['text'],movieRegTupe)
+				spoil(tweet,title,sqlCURSOR)
+				insertDuplicatesTable(tweet['id_str'],sqlCURSOR,sqlCONN)
+				matchedCounter+=1
+			
+
+###############################################################
+
 
 movies =[#should be moved to main.py in the future
 		"The Maze Runner",
@@ -117,7 +148,7 @@ movies =[#should be moved to main.py in the future
 		"Interstellar"
 		"Nightcrawler",
 		"Fury",
-		"Gone Girl",#why the fuck is this in table_T?
+		"Gone Girl",
 		"The Losers",
 		"The Lord of the Rings",
 		"The Wolf of Wall Street",
@@ -164,23 +195,25 @@ movies =[#should be moved to main.py in the future
 #fix double single quotes
 #fix some unicode mistakes
 
-#proposition for making this shit so much faster:
-#instead of searching the same db for n movies n times, search it once for any movie on the list then pick out which one it matched. 
-#i.e. big movie name regex instead of one by one searching. If match, find out if it passes the filter test, then find out which movie it is and the spoiler. 
-#why is this faster than current method?
-#Goes through list once instead of n-movies times. 
-#database searching is slower than regex matching. 
-#faster first result
-#doesn't check in order of movie list
-
-
 #turns out it's faster with more popular movies, not more movies.
+
+
+###############################################################
+
 
 megaRegex = oneRegexToRuleThemAll(moviesToRegexList(movies))
 twitterDB=mongoConnect()
-sqlc = sqlConnect(sqlStart())
-for i in range(10):
-	query(megaRegex,twitterDB,sqlc)
+sqlCONN = sqlStart()
+sqlCURSOR= sqlConnect(sqlCONN)
+noDuplicatesSetup(sqlCONN)
+
+
+for i in range(2):
+	query(megaRegex,twitterDB,sqlCURSOR,sqlCONN)
+	print "Total Matches: ",matchedCounter,"/",howMuchToSearch
+	matchedCounter=0
 
 #print oneRegexToRuleThemAll(moviesToRegexList(movies))
-print "Total Matches: ",matchedCounter,"/",howMuchToSearch
+
+
+###############################################################
